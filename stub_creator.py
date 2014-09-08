c57ba69e566d023121472ef6e3dea3254d070e6e
Leo Suarez <Leo@dtraleigh.com>
2014-09-08 15:25:28 -0400

    Made it usable by others. Added TV and VHS options as well as the ability to add a message to the stub

--- stub_creator.py
+++ stub_creator.py
@@ -3,10 +3,10 @@
 #\\\\
 import os
 
-def new_stub_or_quit():
-    clear = lambda: os.system('cls')
-    clear()
-    answer = raw_input('Create new media stub? (Y or N): ')
+def new_stub_or_quit():    
+    answer = ''
+    while (answer != 'y' and answer != 'Y' and answer != 'n' and answer != 'N'):
+        answer = raw_input('Create new media stub? (Y or N): ')
     
     if answer == 'Y' or answer == 'y':
         return True
@@ -22,8 +22,8 @@ def enter_year():
         
 def enter_movie_type():
     movie_type = ''
-    while(movie_type != '1' and movie_type != '2' and movie_type != '3'):
-        movie_type = raw_input('DVD (1), BLURAY (2), or HDDVD (3)?: ')
+    while(movie_type != '1' and movie_type != '2' and movie_type != '3' and movie_type != '4' and movie_type != '5'):
+        movie_type = raw_input('DVD (1), BLURAY (2),  HDDVD (3), TV (4), or VHS (5)?: ')
         
     if movie_type == '1':
         movie_type = 'DVD'
@@ -31,12 +31,38 @@ def enter_movie_type():
         movie_type = 'BLURAY'
     elif movie_type == '3':
         movie_type = 'HDDVD'
+    elif movie_type == '4':
+        movie_type = 'TV'
+    elif movie_type == '5':
+        movie_type = 'VHS'
         
     return movie_type
 
+def add_message():
+    answer = ''
+    while (answer != 'y' and answer != 'Y' and answer != 'n' and answer != 'N'):
+        answer = raw_input('Add a message to the media stub? (Y or N): ')
+    
+    if answer == 'Y' or answer == 'y':
+        message = raw_input('Please type your message: ')
+        return message
+    elif answer == 'N' or answer == 'n':
+        return False
+
 def create_stub_content(title, movie_type):
-    # <discstub><title>Title (year)</title></discstub>
-    stub = '<discstub><title>' + title + ' (' + movie_type + ')</title></discstub>'
+    #<discstub>
+    #    <title>Title (year)</title>
+    #    <message>Message to be displayed</message>
+    #</discstub>
+    indent = '    '
+    
+    message = add_message()
+    
+    stub = '<discstub>\n'
+    stub = stub + indent + '<title>' + title + ' (' + movie_type + ')</title>\n'
+    if message:
+        stub = stub + indent + '<message>' + message + '</message>\n'
+    stub = stub + '</discstub>\n'
     
     return stub
 
@@ -53,9 +79,16 @@ def ask_and_create():
     the_file = open(os.path.join(sub_dir, filename), 'w')
     the_file.write(stub_content)
     the_file.close()
+    
+    print '\nCreated file: "' + filename + '\"'
+    print stub_content
+
+clear = lambda: os.system('cls')
+clear()
 
 user_input = new_stub_or_quit()
 
 while user_input:
     ask_and_create()
     user_input = new_stub_or_quit()
+    clear()
