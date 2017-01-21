edf89e519da2675555f817374bdf2a224eb26d86
Leo Suarez <Leo@DTRaleigh.com>
2017-01-21 14:22:54 -0500

    Created an environment class so that changes to the output folder are seperate from each media stub object.

--- stub_creator.py
+++ stub_creator.py
@@ -1,16 +1,12 @@
-#////
-#Script to ask a user a few things and then create a media stub for Kodi
-#\\\\
+# ////
+# Script to ask a user a few things and then create a media stub for Kodi
+# \\\\
 import os
 
-class new_media_stub():
 
+class environment():
     def __init__(self):
         self.sub_dir = "../Movies/"
-        self.title = ""
-        self.year = ""
-        self.type = ""
-        self.message = ""
 
     def instructions(self):
         menu = "Stub Creator 'Stubby' | Welcome!\n"
@@ -22,12 +18,12 @@ class new_media_stub():
 
         return menu
 
-    def clear_screen(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
-
     def change_output_folder(self):
         self.sub_dir = input("Enter output dir (. for current, .. for up one level, etc.): ")
         self.clear_screen()
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
 
     def new_stub_or_quit(self):
         print(self.instructions())
@@ -45,18 +41,46 @@ class new_media_stub():
         elif answer == "Q" or answer == "q":
             return False
 
+
+class new_media_stub():
+    def __init__(self):
+        self.title = ""
+        self.year = ""
+        self.type = ""
+        self.message = ""
+
+    def ask_and_create(self, env):
+        self.get_title()
+        self.get_year()
+        self.get_type()
+        self.get_message()
+
+        stub_content = self.create_stub_content()
+
+        filename = self.title + " (" + self.year + ")." + self.type + ".disc"
+
+        if not os.path.exists(env.sub_dir):
+            os.makedirs(env.sub_dir)
+
+        the_file = open(os.path.join(env.sub_dir, filename), "w")
+        the_file.write(stub_content)
+        the_file.close()
+
+        print('\nCreated file: "' + filename + '\"')
+        print(stub_content)
+
     def get_title(self):
         self.title = input("What is the movie title?: ")
 
     def get_year(self):
-        while(len(self.year) != 4):
+        while (len(self.year) != 4):
             self.year = input("What year was it released? (4 digits): ")
 
     def get_type(self):
         valid_choices = ["1", "2", "3", "4", "5"]
 
         movie_type = ""
-        while(movie_type not in valid_choices):
+        while (movie_type not in valid_choices):
             movie_type = input("DVD (1), BLURAY (2),  HDDVD (3), TV (4), or VHS (5)?: ")
 
         if movie_type == "1":
@@ -81,10 +105,10 @@ class new_media_stub():
             self.message = input("Please type your message: ")
 
     def create_stub_content(self):
-        #<discstub>
+        # <discstub>
         #    <title>Title (year)</title>
         #    <message>Message to be displayed</message>
-        #</discstub>
+        # </discstub>
         indent = "    "
 
         stub = "<discstub>\n"
@@ -95,40 +119,20 @@ class new_media_stub():
 
         return stub
 
-    def ask_and_create(self):
-        self.get_title()
-        self.get_year()
-        self.get_type()
-        self.get_message()
-
-        stub_content = self.create_stub_content()
-
-        filename = self.title + " (" + self.year + ")." + self.type + ".disc"
-
-        if not os.path.exists(self.sub_dir):
-            os.makedirs(self.sub_dir)
-
-        the_file = open(os.path.join(self.sub_dir, filename), "w")
-        the_file.write(stub_content)
-        the_file.close()
-
-        print('\nCreated file: "' + filename + '\"')
-        print(stub_content)
-
 
 try:
     input = raw_input
 except NameError:
     pass
 
-media_stub = new_media_stub()
-user_input = media_stub.new_stub_or_quit()
+env = environment()
+user_input = env.new_stub_or_quit()
 
 while user_input != False:
     if user_input == "c":
         media_stub = new_media_stub()
-        media_stub.ask_and_create()
+        media_stub.ask_and_create(env)
     elif user_input == "s":
-        media_stub.change_output_folder()
-    user_input = media_stub.new_stub_or_quit()
-    media_stub.clear_screen()
+        env.change_output_folder()
+    user_input = env.new_stub_or_quit()
+    env.clear_screen()
